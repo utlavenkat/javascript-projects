6bf0ab6c524c535fee232c6cff5fd3a872df394d
Venkat Utla <utla.venkat@gmail.com>
2026-06-13 13:48:47 +0530

    Feature development (#4)
    * Added live server
    
    * Modal project

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 # Use nginx lightweight web server
 FROM nginx:alpine
 
-# Copy index.html to the nginx web root
-COPY index.html /usr/share/nginx/html/
+# Copy entire project directory to the nginx web root
+COPY . /usr/share/nginx/html/
 
 # Expose port 80
 EXPOSE 80
---
+++ Modal/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const modalBtns = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal')
+const closeModalBtn = document.querySelector('.close-modal')
+const overlay = document.querySelector('.overlay')
+
+const showModal = function() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden')
+}
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden')
+}
+
+for(let i=0; i< modalBtns.length; i++) {
+    modalBtns[i].addEventListener('click',showModal)
+}
+
+closeModalBtn.addEventListener('click',closeModal)
+overlay.addEventListener('click', closeModal)
+
+document.addEventListener('keydown', function(e){
+    if(e.key ==='Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+})
